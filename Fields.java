f8f899e034550fff4b525ae443ea6461da1a662b
August Shi <awshi2@illinois.edu>
2019-09-20 10:05:54 -0500

    Initial stuff

---
+++ Fields.java
@@ -0,0 +1,38 @@
+public class Fields {
+    public int field87;
+    public int field60;
+    public int field73;
+    public int field3;
+    public int field22;
+    public int field76;
+    public int field41;
+    public int field63;
+    public int field7;
+    public int field2;
+    public int field45;
+    public int field75;
+    public int field72;
+    public int field93;
+    public int field24;
+    public int field65;
+    public int field74;
+    public int field44;
+    public int field51;
+    public int field6;
+    public int field55;
+    public int field29;
+    public int field80;
+    public int field85;
+    public int field70;
+    public int field56;
+    public int field90;
+    public int field38;
+    public int field31;
+    public int field84;
+
+    public static void main(String[] args) throws Exception {
+        for (java.lang.reflect.Field f : Fields.class.getDeclaredFields()) {
+            System.out.println(f);
+        }
+    }
+}
---
+++ Methods.java
@@ -0,0 +1,38 @@
+public class Methods {
+    public void meth34() {}
+    public void meth17() {}
+    public void meth16() {}
+    public void meth37() {}
+    public void meth45() {}
+    public void meth19() {}
+    public void meth68() {}
+    public void meth91() {}
+    public void meth44() {}
+    public void meth87() {}
+    public void meth9() {}
+    public void meth23() {}
+    public void meth54() {}
+    public void meth8() {}
+    public void meth94() {}
+    public void meth0() {}
+    public void meth48() {}
+    public void meth70() {}
+    public void meth53() {}
+    public void meth56() {}
+    public void meth46() {}
+    public void meth72() {}
+    public void meth32() {}
+    public void meth96() {}
+    public void meth42() {}
+    public void meth25() {}
+    public void meth67() {}
+    public void meth74() {}
+    public void meth11() {}
+    public void meth39() {}
+
+    public static void main(String[] args) throws Exception {
+        for (java.lang.reflect.Method m : Methods.class.getDeclaredMethods()) {
+            System.out.println(m);
+        }
+    }
+}
